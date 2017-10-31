536d7b4690a212ef25079db19fb698e9bb23aeba
AndyDeany <oneandydean@hotmail.com>
2017-10-31 20:24:43 +0000

    Add placeholder MainMenu view for IntroView to switch to

--- lib/views/__init__.py
+++ lib/views/__init__.py
@@ -1,2 +1,3 @@
 """Module containing all View classes for Otherworld Blight."""
+from .main_menu import MainMenu
 from .intro import IntroView
---
+++ lib/views/main_menu.py
@@ -0,0 +1,17 @@
+"""Module containing the MainMenu view for representing the main menu."""
+from pygametemplate import View
+
+
+class MainMenu(View):
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def logic(self):
+        pass
+
+    def draw(self):
+        pass
