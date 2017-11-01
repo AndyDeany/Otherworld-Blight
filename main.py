f4394ec55081c20b0e502b598814ea04fffc7336
oneandydean@hotmail.com <oneandydean@hotmail.com>
2017-11-01 09:07:20 +0000

    Set window to 1920x1080 fullscreen in main.py, as this is the resolution of the assets we currently have

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from lib.otherworld_blight import OtherworldBlight
 from lib.views import IntroView
 
 
+RESOLUTION = (1920, 1080)
+MODE = "fullscreen"
 CAPTION = "Otherworld Blight v1.0.0"
 
 
 if __name__ == "__main__":
-    game = OtherworldBlight(IntroView, caption=CAPTION, icon="game_icon.png")
+    game = OtherworldBlight(IntroView, RESOLUTION, MODE,
+                            caption=CAPTION, icon="game_icon.png")
     game.run()
