e9f10b559f989e6d4d0b109f545c6ce7afdba065
oneandydean@hotmail.com <oneandydean@hotmail.com>
2017-11-01 08:48:31 +0000

    Add basic intro page using fonts (pygame.movie is deprecated and removed ):

--- lib/views/intro.py
+++ lib/views/intro.py
@@ -1,17 +1,38 @@
 """Module containing the Intro view for playing out the intro sequence."""
-from pygametemplate import View
+import pygame
+from pygametemplate import View, load_font
 
+from lib.views import MainMenu
+
+pygame.init()
 
 class IntroView(View):
 
+    background_colour = (255, 255, 255)
+    text_colour = (0, 0, 0)
+    # TODO: Change the below to use pygametemplate.load_font() when it's fixed.
+    font = pygame.font.Font("assets/fonts/chewy.ttf", 80)
+    intro_text = "Leviathan Operations"
+
     def load(self):
-        pass
+        width, height = self.font.size(self.intro_text)
+        self.coords = ((self.game.width - width)//2, (self.game.height - height)//2)
 
     def unload(self):
         pass
 
     def logic(self):
-        pass
+        for key in ("escape", " ", "enter", "numpadenter"):
+            if self.game.input.buttons[key].pressed:
+                self.game.switch_view(MainMenu)
+
+        try:
+            current_text = self.intro_text[:self.game.frame//10]
+        except IndexError:  # Intro has finished
+            self.game.switch_view(MainMenu)
+
+        self.text = self.font.render(current_text, True, self.text_colour)
 
     def draw(self):
-        pass
+        self.game.screen.fill(self.background_colour)
+        self.game.screen.blit(self.text, self.coords)
