dd538b6d7df408f554cd662d8b6723c9125e6ab2
oneandydean@hotmail.com <oneandydean@hotmail.com>
2017-11-05 21:40:42 +0100

    Add code for Controls view

--- lib/views/controls.py
+++ lib/views/controls.py
@@ -1,17 +1,25 @@
 """Module containing the Controls view for showing the controls."""
-from pygametemplate import View
+from pygametemplate import View, Image
 
 
 class Controls(View):
 
+    background = Image("menus/background.png")
+    controls_image = Image("menus/controls.png")
+    images = [background, controls_image]
+
     def load(self):
-        pass
+        for image in self.images:
+            image.load()
 
     def unload(self):
-        pass
+        for image in self.images:
+            image.unload()
 
     def logic(self):
-        pass
+        if any(self.game.input.buttons[key].pressed for key in ("escape", "backspace")):
+            self.game.set_view("MainMenu")
 
     def draw(self):
-        pass
+        self.background.display(self.game.screen, (0, 0))
+        self.controls_image.display(self.game.screen, (0, 0))
